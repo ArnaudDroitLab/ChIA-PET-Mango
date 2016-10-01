17c86a2b403c25e4d60b25418cb835e54739cf64
Eric Fournier <Fournier.Eric.2@crchudequebec.ulaval.ca>
2016-10-01 14:40:27 -0400

    Bug fixes. Allow recovery at all steps after failed run.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -33,6 +33,9 @@ do
         species="$2"
         shift # past argument
         ;;    
+        -l|--forcelinker)
+        forcelinker="Y"
+        ;;            
         *)
                 # unknown option
         ;;
@@ -43,7 +46,7 @@ done
 case $species in
     hg19)
     bowtieindex=/is1/commonDatasets/mugqic_space/genomes/species/Homo_sapiens.hg19/Sequence/BowtieIndex/genome
-    genomefasta=/is1/commonDatasets/mugqic_space/genomes/species/Homo_sapiens.hg19/Sequence/WholeGenomeFasta/genome.fa
+    genomefasta=/is1/commonDatasets/mugqic_space/genomes/species/Homo_sapiens.hg19/Sequence/WholeGenomeFasta/genome.fa.fai
     chrMName=chrM
     ;;
     hg38)
@@ -57,28 +60,41 @@ case $species in
 esac
 
 
-module load R/R-3.3.0 python/2.7.8 parallel/20140922
+module load R/R-3.3.0_BioC-3.3 python/2.7.8 parallel/20140922
 
+# Convert the BAM file into fastq files #################################################
+
+# If the input is a BAM file, convert it to two fastq files.
 mkdir -p $prefix/fastq
 if [[ "$bam" != "" ]]
 then
     # Find out the sample identifier.
     id=$(basename $bam .bam)
 
-    # Split the bam file into two FASTQ files.
-#    java -jar $PICARD_PATH SamToFastq I=$bam F=$prefix/fastq/$id.R1.fastq.gz F2=$prefix/fastq/$id.R2.fastq.gz
-    
-    # Set the input fastq file names.
+    # Determine the output file names.
     fastq1=$prefix/fastq/$id.R1.fastq.gz
     fastq2=$prefix/fastq/$id.R2.fastq.gz
-fi
 
+    # If the output file already exist, do not regenerate them.
+    if [ ! -e $fastq1 ] || [ ! -e $fastq2 ]
+    then
+        # Split the bam file into two FASTQ files.    
+        echo "Extracting fastq from bam file."
+        java -jar $PICARD_PATH SamToFastq I=$bam \
+           F=$prefix/fastq/$id.R1.fastq.gz \
+           F2=$prefix/fastq/$id.R2.fastq.gz &
+    fi
+fi
+wait
+
+# Figure out the fastq file basenames. ########################################
 gzregex="(.*\/)*(.*)(\.fq|\.fastq)(\.gz)*$"
 if [[ $fastq1 =~ $gzregex ]]
 then
     id1=${BASH_REMATCH[2]}
 else
     echo "Could not detect fastq1 file type. Aborting."
+    exit
 fi
 
 if [[ $fastq2 =~ $gzregex ]]
@@ -86,29 +102,85 @@ then
     id2=${BASH_REMATCH[2]}
 else
     echo "Could not detect fastq2 file type. Aborting."
+    exit
 fi
 
-mkdir -p $prefix/cut
-#~/.local/bin/cutadapt -a ACGCGATATCTTATCTGACT -A AGTCAGATAAGATATCGCGT \
-#                      --minimum-length 17 --overlap 10 -o $prefix/cut/$id1.fastq -p $prefix/cut/$id2.fastq \
-#                      $fastq1 $fastq2
 
-rm ${prefix}/mango/mango_1.same.fastq
-rm ${prefix}/mango/mango_2.same.fastq
-ln -s `pwd`/$prefix/cut/$id1.fastq ${prefix}/mango/mango_1.same.fastq
-ln -s `pwd`/$prefix/cut/$id2.fastq ${prefix}/mango/mango_2.same.fastq
+# Remove linker sequences #####################################################
+mkdir -p $prefix/cut
+
+cut1=$prefix/cut/$id1.fastq.gz
+cut2=$prefix/cut/$id2.fastq.gz
+if [ ! -e $cut1 ] || [ ! -e $cut2 ]
+then
+    echo "Removing linker sequences.."
+    
+    # Are we keeping only trimmed reads?
+    extraoptions=""
+    if [[ "$forcelinker" == "Y" ]]
+    then
+        extraoptions="--discard-untrimmed"
+    fi
+    
+    ~/.local/bin/cutadapt -a ACGCGATATCTTATCTGACT -A AGTCAGATAAGATATCGCGT \
+                          --minimum-length 17 --overlap 10 $extraoptions \
+                          -o $cut1 -p $cut2 $fastq1 $fastq2 &
+fi
+wait
+
+
+# Perform alignment #####################################################
+
+# Mango can generate alignments itself, but it is limited to 1 thread, which
+# makes the process painfully slow. We'll do it outside of mango, using the same
+# parameters.
+# Mango uses the mapq 40 bowtie parameter to set the MAPQ of uniquely mapped reads to
+# a specific value. This option is not present in alternate aligners, some
+# we're stuck with bowtie1 unless we modify mango itself.
+mkdir -p $prefix/alignment
+align1=$prefix/alignment/$id1.sam
+align2=$prefix/alignment/$id2.sam
+for alignout in $align1 $align2
+do
+    if [ ! -e $alignout ]
+        fastqid=`basename $alignout .sam`
+        gzip -dc $prefix/cut/$fastqid.fastq.gz |
+            bowtie/bowtie-1.1.2/bowtie -S -n 2 -l 50 -k 1 --chunkmbs 500 --sam-nohead \
+                                   --mapq 40 -m 1 --best --threads 8 --phred33-quals \
+                                   $bowtieindex \
+                                   - \
+                                   $alignout &
+    fi
+done
+wait
+
+#java -jar $PICARD_PATH SortSam I=$align1 O=$align1.sorted SORT_ORDER=queryname &
+#java -jar $PICARD_PATH SortSam I=$align2 O=$align2.sorted SORT_ORDER=queryname &
+sed -i -e 's/\/1//' `pwd`/$align1 &
+sed -i -e 's/\/2//' `pwd`/$align2 &
+wait
+
+sort $align1 | sort -t: -k 7 -n > $align1.sorted &
+sort $align2 | sort -t: -k 7 -n > $align2.sorted &
+wait
+
+# Generate symbolic links for the sam files.
+ln -s `pwd`/$align1.sorted ${prefix}/mango/mango_1.same.sam
+ln -s `pwd`/$align2.sorted ${prefix}/mango/mango_2.same.sam
+
+
                       
-                      
+# Run mango #########################################################
 mkdir -p $prefix/mango
 Rscript mango/mango/mango.R --bedtoolspath bedtools/bedtools2/bin/bedtools \
                             --bowtiepath bowtie/bowtie-1.1.2/bowtie \
-                            --fastq1 $prefix/cut/$id1.fastq \
-                            --fastq2 $prefix/cut/$id2.fastq \
+                            --fastq1 $fastq1 \
+                            --fastq2 $fastq2 \
                             --outdir $prefix/mango \
                             --bowtieref $bowtieindex \
                             --bedtoolsgenome $genomefasta \
                             --chromexclude $chrMName,chrY \
-                            --stages 2:5 \
+                            --stages 3:5 \
                             --shortreads FALSE \
                             --peakslop 1500 \
                             --reportallpairs TRUE
